ab11eb06486f2c9eb0c6bf07ef35a66e1a499424
Kyle Conkright <kyleconkright@gmail.com>
2014-03-31 09:56:05 -0700

    Replaced Lemonade with Bootstrap Grid

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
 		cssmin: {
 		  	combine: {
 		    	files: {
-		      		'tmp/globalmin.css': ['lib/vendor/lemonade.css', 'tmp/global.css']
+		      		'tmp/globalmin.css': ['lib/vendor/bootstrap.css', 'tmp/global.css']
 		    	}
 		  	}
 		},
