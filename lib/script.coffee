04cc1b812ec4f40bf199acfefd92fc7218335d1b
Kyle Conkright <kyleconkright@gmail.com>
2014-04-07 11:00:45 -0700

    New Version with Lemonade Grid
    I hate bootstrap

--- lib/script.coffee
+++ lib/script.coffee
@@ -8,8 +8,17 @@ $ ->
 			$.each results, ->
 				date = this.datetime.split "T"
 				show = date[0].split "-"
-				console.log(date[1])
-				$('<div class="col-md-2">' + show[1] + '-' + show[2] + '-' + show[0] + '</div><div class="col-md-7"><span class="city">' + this.venue.city + ', ' + this.venue.region + '</span> - ' + this.venue.name + '</div></div><div class="col-md-3"><a href="' + this.ticket_url + '" target="_blank" class="tickets"><i class="fa fa-ticket"></i> tickets</a></div>').appendTo('div.shows')
+				tickets = '<a href="' + this.ticket_url + '" target="_blank" class="tickets"> tickets <i class="fa fa-ticket"></i></a>'
+				if this.venue.region is "MN"
+					soldout = 'Sold Out'
+				else
+					soldout = ''
+
+				$('<div class="frame"><div class="bit-6">' + show[1] + '-' + show[2] + '-' + show[0] + '</div>
+					<div class="bit-2"><span class="city">' + this.venue.city + ', ' + this.venue.region + '</span> - ' + this.venue.name + '</div>
+					<div class="bit-6"><span class="so">' + soldout + '</span></div>
+					<div class="bit-6">' + tickets + '</div></div>')
+					.appendTo('div.shows')
 
 	# $.ajax
 	# 	url: 'data/data.json'
--- tmp/script.js
+++ tmp/script.js
@@ -6,11 +6,16 @@
       dataType: 'jsonp',
       success: function(results) {
         return $.each(results, function() {
-          var date, show;
+          var date, show, soldout, tickets;
           date = this.datetime.split("T");
           show = date[0].split("-");
-          console.log(date[1]);
-          return $('<div class="col-md-2">' + show[1] + '-' + show[2] + '-' + show[0] + '</div><div class="col-md-7"><span class="city">' + this.venue.city + ', ' + this.venue.region + '</span> - ' + this.venue.name + '</div></div><div class="col-md-3"><a href="' + this.ticket_url + '" target="_blank" class="tickets"><i class="fa fa-ticket"></i> tickets</a></div>').appendTo('div.shows');
+          tickets = '<a href="' + this.ticket_url + '" target="_blank" class="tickets"> tickets <i class="fa fa-ticket"></i></a>';
+          if (this.venue.region === "MN") {
+            soldout = 'Sold Out';
+          } else {
+            soldout = '';
+          }
+          return $('<div class="frame"><div class="bit-6">' + show[1] + '-' + show[2] + '-' + show[0] + '</div> <div class="bit-2"><span class="city">' + this.venue.city + ', ' + this.venue.region + '</span> - ' + this.venue.name + '</div> <div class="bit-6"><span class="so">' + soldout + '</span></div> <div class="bit-6">' + tickets + '</div></div>').appendTo('div.shows');
         });
       }
     });
